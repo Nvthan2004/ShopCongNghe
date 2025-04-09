9c7ecfe6a7c29ce8df38aea87a2b68e55441e416
DuyChili <huynhvanduyk20@gmail.com>
2025-04-09 16:16:44 +0700

    commit

--- example-app/storage/framework/views/a2fb07056020781323988e50433c0fef.php
+++ example-app/storage/framework/views/a2fb07056020781323988e50433c0fef.php
@@ -20,7 +20,7 @@
     <table class="table table-bordered">
         <thead class="table-dark">
             <tr>
-                <th>#</th>
+                <th style="display: none;">#</th>
                 <th>Name</th>
                 <th>Slug</th>
                 <th>Logo</th>
@@ -30,7 +30,7 @@
         <tbody>
             <?php $__empty_1 = true; $__currentLoopData = $brands; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $brand): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); $__empty_1 = false; ?>
             <tr>
-                <td><?php echo e($loop->iteration); ?></td>
+                <td style="display: none;"><?php echo e($loop->iteration); ?></td>
                 <td><?php echo e($brand->name); ?></td>
                 <td><?php echo e($brand->slug); ?></td>
                 <td>
