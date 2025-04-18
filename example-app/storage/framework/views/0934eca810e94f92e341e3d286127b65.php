67a5778ef4948e8365e6cf016f1c712fa582f39b
NgoTrongHieu201004 <ngohieu201004@gmail.com>
2025-04-18 21:06:56 +0700

    update du lieu

--- example-app/storage/framework/views/0934eca810e94f92e341e3d286127b65.php
+++ example-app/storage/framework/views/0934eca810e94f92e341e3d286127b65.php
@@ -7,6 +7,12 @@
     <?php if(session('success')): ?>
     <div class="alert alert-success"><?php echo e(session('success')); ?></div>
     <?php endif; ?>
+    <?php if(session('error')): ?>
+        <div class="alert alert-danger">
+        <?php echo e(session('error')); ?>
+
+        </div>
+    <?php endif; ?>
 
     <!-- Add Brand Button -->
     <div class="mb-3">
